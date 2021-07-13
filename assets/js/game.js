10c9090cd97b93ff8d93d707e155c5dfebd85e70
Bailey Poe <baileypoe1222@gmail.com>
2021-07-13 17:27:19 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,6 +63,10 @@ var enemyAttack = 12;
     };
     };
  };
+ var startGame = function() {
+     playerHealth = 100;
+     playerAttack = 10;
+     playerMoney = 10;
  for(var i = 0; i < enemyNames.length; i++) {
      if (playerHealth > 0) {
          window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
@@ -74,5 +78,20 @@ var enemyAttack = 12;
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
- };
+ }
+ endGame();
+};
+var endGame = function() {
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".")
+    } else {
+        window.alert("You've lost your robot in battle")
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if(playAgainConfirm) {
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+    }
 
+};
