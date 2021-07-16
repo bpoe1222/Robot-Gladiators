0f8d0dd63b28435f6171bfd0690329ead87df417
Bailey Poe <baileypoe1222@gmail.com>
2021-07-16 13:57:28 -0500

    added random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,13 +15,17 @@ var enemyAttack = 12;
     // Alert players that they are starting the round
     window.alert("The fight has begun!");
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth- damage);
     //Log a resulting message to the console so we know that it worked
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName +" now has " + enemyHealth + " health remaining."
     );
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth variable'
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack-3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage)
     //Log a resulting message to the console so we know that it worked
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -52,7 +56,7 @@ var enemyAttack = 12;
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             
             //subtract money from 'playerMoney' for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         } else {
@@ -76,7 +80,7 @@ var enemyAttack = 12;
          break;
      }
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = Math.floor(Math.random() * 21) + 40;
     fight(pickedEnemyName);
  }
  endGame();
@@ -95,3 +99,8 @@ var endGame = function() {
     }
 
 };
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1)) + 40;
+
+    return value;
+}
